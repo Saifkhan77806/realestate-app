3f23802f36b603793a5e4aad4e241a9f71fa2b2f
saifkhan77806 <saifkhan77806@gmail.com>
2025-10-11 19:39:48 +0530

    project setup is ready

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-center font-rubikSemiBold">
+        app/index.tsx t edit this screen.
+      </Text>
+      <Link href={"/sign-in"}>Sign in</Link>
+      <Link href={"/explore"}>Explore</Link>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily:{
+        rubik: ["Rubik-Regular", "sans-serif"],
+        "rubikBold": ["Rubik-Bold", "sans-serif"],
+        "rubikMedium": ["Rubik-Medium", "sans-serif"],
+        "rubikLight": ["Rubik-Light", "sans-serif"],
+        "rubikExtraBold": ["Rubik-ExtraBold", "sans-serif"],
+        "rubikSemiBold": ["Rubik-SemiBold", "sans-serif"],
+        "rubikRegular": ["Rubik-Regular", "sans-serif"],
+      },
+      colors:{
+        "primary": {
+          100: "#0061ff0a",
+          200: "#0061ff1a",
+          300: "#0061ff2a",
+        },
+        accent: {
+          100: "#fbfbfd"
+        },
+        black: {
+          DEFUALT: "#000000",
+          100: "#8c8e98",
+          200: "#666876",
+          300: "#191d31",
+        },
+        danger: "#f75555"
+
+      }
+    },
+  },
+  plugins: [],
+};
